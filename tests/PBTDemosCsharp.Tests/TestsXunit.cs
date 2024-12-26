e0467cdcfcbf1ea1cf9ed7961f1193101257592d
Patrick Drechsler <patrick@pdrechsler.de>
2024-12-26 21:38:20 +0100

    ♻️ chore: rename c# unit test file and add hello world

---
+++ tests/PBTDemosCsharp.Tests/TestsXunit.cs
@@ -0,0 +1,14 @@
+﻿using FsCheck.Xunit;
+
+namespace PBTDemosCsharp.Tests;
+
+public class TestsXunit
+{
+  [Property]
+  public bool Reversing_a_list_twice_gives_the_original_list(List<int> list)
+  {
+    var actual = list.AsEnumerable().Reverse().Reverse();
+    var expected = list;
+    return actual.SequenceEqual(expected);
+  }
+}
