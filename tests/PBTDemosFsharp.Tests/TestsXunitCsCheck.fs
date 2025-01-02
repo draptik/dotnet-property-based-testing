647234739363908af5302dfe9030f0127c8846e2
Patrick Drechsler <patrick@pdrechsler.de>
2025-01-02 01:08:59 +0100

    ⚗️ cscheck: usage in f# unclear (does not compile)

--- tests/PBTDemosFsharp.Tests/TestsXunitCsCheck.fs
+++ tests/PBTDemosFsharp.Tests/TestsXunitCsCheck.fs
@@ -1,5 +1,6 @@
 module TestsXunitCsCheck
 
+open System
 open CsCheck
 open Swensen.Unquote
 open Xunit
@@ -8,3 +9,25 @@ open Xunit
 let ``generated numbers are in scope`` () =
   let gen = Gen.Int.Positive.Where(fun x -> x < 100)
   gen.Sample(fun s -> test <@ s < 100 @>)
+
+type MyClass = {
+  MyInt: int
+  MyString: string
+}
+
+[<Fact>]
+let ``each input must be correct`` () =
+
+  // let toCSharpFunc (f: Gen<'a> -> bool) : Func<Gen<'a>, bool> =
+  //   Func<Gen<'a>, bool>(f)
+
+  let myClassGenerator : Gen<MyClass> =
+      Gen.Select(
+        Gen.Int,
+        Gen.String[10, 20],
+        fun (i, s) -> { MyInt = i; MyString = s })
+
+  let gs = myClassGenerator.List
+
+  let f a : unit = ()
+  Check.Sample(gs, f)
