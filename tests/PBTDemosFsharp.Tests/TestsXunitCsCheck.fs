7e214f0c2a87beb6d960178c7a926c940f73ff89
Patrick Drechsler <patrick@pdrechsler.de>
2025-01-07 17:14:38 +0100

    🚨 chore(lint): reformat

--- tests/PBTDemosFsharp.Tests/TestsXunitCsCheck.fs
+++ tests/PBTDemosFsharp.Tests/TestsXunitCsCheck.fs
@@ -10,10 +10,7 @@ let ``generated numbers are in scope`` () =
   let gen = Gen.Int.Positive.Where(fun x -> x < 100)
   gen.Sample(fun s -> test <@ s < 100 @>)
 
-type MyClass = {
-  MyInt: int
-  MyString: string
-}
+type MyClass = { MyInt: int; MyString: string }
 
 [<Fact>]
 let ``each input must be correct`` () =
@@ -27,21 +24,14 @@ let ``each input must be correct`` () =
   //       Gen.String[10, 20],
   //       mapToCSharpFunc (fun i s -> { MyInt = i; MyString = s }))
 
-  let myClassGenerator : Gen<MyClass> =
-      Gen.Select(
-        Gen.Int.Positive,
-        Gen.String[10, 20],
-        Func<int, string, MyClass>(fun i s -> { MyInt = i; MyString = s }))
+  let myClassGenerator: Gen<MyClass> =
+    Gen.Select(Gen.Int.Positive, Gen.String[10, 20], Func<int, string, MyClass>(fun i s -> { MyInt = i; MyString = s }))
 
   myClassGenerator.List.Sample(fun x ->
     if x.Count = 0 then
       true = true
     else
       let actual =
-        x.TrueForAll(fun el ->
-          el.MyInt > 0 &&
-          el.MyString.Length >= 10 &&
-          el.MyString.Length <= 20
-        )
-      actual = true
-  )
+        x.TrueForAll(fun el -> el.MyInt > 0 && el.MyString.Length >= 10 && el.MyString.Length <= 20)
+
+      actual = true)
