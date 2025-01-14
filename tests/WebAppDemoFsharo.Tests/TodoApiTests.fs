3ab5205842753a87f82b288bf9c92dd5db938236
Patrick Drechsler <patrick@pdrechsler.de>
2025-01-14 22:52:53 +0100

    🧪 test(web-api): async pbt test also fails when using f#

--- tests/WebAppDemoFsharo.Tests/TodoApiTests.fs
+++ tests/WebAppDemoFsharo.Tests/TodoApiTests.fs
@@ -5,6 +5,8 @@ open System.Net
 open System.Net.Http.Json
 open Microsoft.AspNetCore.Mvc.Testing
 open Xunit
+open FsCheck
+open FsCheck.Xunit
 
 type Request = {
   Title: string
@@ -34,7 +36,38 @@ let ``create todo item should return created`` () =
     Assert.Equal(HttpStatusCode.Created, response.StatusCode)
 
     let! responseBody = response.Content.ReadFromJsonAsync<TodoItem>()
-    let unparsedGuid = responseBody.Id.Value.ToString()
-    let maybeId = tryParseGuid unparsedGuid
+    let unparsedId = responseBody.Id.Value.ToString()
+    let maybeId = tryParseGuid unparsedId
+    Assert.True(maybeId.IsSome, "Expected Some but got None.")
+  }
+
+[<Property(Skip = "Does not work")>]
+let ``create todo item should handle valid input`` (t: NonWhiteSpaceString, d: NonWhiteSpaceString) =
+  let title = t.Get
+  let details = d.Get
+
+  let tryParseGuid (input: string) =
+    match Guid.TryParse(input) with
+    | true, parsedGuid -> Some parsedGuid
+    | false, _ -> None
+
+  task {
+    // Arrange
+    use factory = new WebApplicationFactory<Program>()
+    use client = factory.CreateClient()
+
+    let url = "/api/todo"
+    let request = { Title = title; Details = details }
+
+    // Act
+    let! response = client.PutAsJsonAsync(url, request)
+
+    // Assert
+    Assert.True(response.IsSuccessStatusCode, $"Expected success status code but got %A{response.StatusCode}")
+    Assert.Equal(HttpStatusCode.Created, response.StatusCode)
+
+    let! responseBody = response.Content.ReadFromJsonAsync<TodoItem>()
+    let unparsedId = responseBody.Id.Value.ToString()
+    let maybeId = tryParseGuid unparsedId
     Assert.True(maybeId.IsSome, "Expected Some but got None.")
   }
