be71314b3229136f965e357694512c96fdcb134d
Patrick Drechsler <patrick@pdrechsler.de>
2025-04-12 22:49:50 +0200

    ✨ feat(music): add song recommendation demo

---
+++ src/SongRecommendations/ISongService.cs
@@ -0,0 +1,11 @@
+﻿namespace SongRecommendations;
+
+public interface ISongService
+{
+  Task<IReadOnlyCollection<User>> GetTopListenersAsync(int songId);
+  Task<IReadOnlyCollection<Scrobble>> GetTopScrobblesAsync(string userName);
+}
+
+public sealed record Song(int Id, bool IsVerifiedArtist, byte Rating);
+public sealed record Scrobble(Song Song, int ScrobbleCount);
+public sealed record User(string UserName, int TotalScrobbleCount);
